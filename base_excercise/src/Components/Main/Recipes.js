ec880badd61db36fde74a3e8d5fc2bd89a63bae8
Laura <laura.jarventie@edu.bc.fi>
2021-04-19 15:20:25 +0300

    added db for recepies and fetch it in recepies excercise

--- base_excercise/src/Components/Main/Recipes.js
+++ base_excercise/src/Components/Main/Recipes.js
@@ -1,11 +1,33 @@
 import React from "react";
 
-const Recipes = () => {
+const Recipes = ({ recepies }) => {
   return (
     <div>
-      <h1>Here come´s recipes soon....</h1>
+      <h1>Here come´s recipies soon....</h1>
+      <ul>
+        {recepies.map((recipe) => {
+          return (
+            <li key={recipe.name}>
+              {recipe.recipeCategory}
+              {recipe.recipeIngredient}
+              {recipe.recipeInstructions}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
 
+class App extends Recipes {
+  state = {
+    notes: [],
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3001/users")
+      .then((resp) => resp.json())
+      .then((data) => this.setState({ recepies: data }));
+  }
+}
 export default Recipes;
